4701850bbdbefdc77001a5906e1e0c26fbc1e644
Ramon Blanquer <blanquer.ramon@gmail.com>
2018-03-11 14:51:07 +0000

    Using nodemon to reload frontend tree.

--- frontend/server.js
+++ frontend/server.js
@@ -1,5 +1,17 @@
 const express = require('express')
+const multer = require('multer')
 const app = express()
+
+var storage = multer.diskStorage({
+  destination: function(req, file, callback) {
+    callback(null, './uploads');
+  },
+  filename: function(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  }
+});
 
 app.get('/', (req, res) => res.send("Hello from frontend!"))
 app.listen(3000, () => console.log(`Example app listening! NODE_ENV: ${process.env.NODE_ENV}`))
+
+
