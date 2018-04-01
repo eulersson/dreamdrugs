2cbee4bc35b78ef7081f29aa328daf0516e2e5e3
Ramon Blanquer <blanquer.ramon@gmail.com>
2018-04-01 21:40:55 +0100

    Camera working.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,54 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
-    this.state = { impath: '' };
+    this.state = { impath: '' }
     this.handleUpload = this.handleUpload.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    function hasGetUserMedia() {
+      return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+    }
+
+    if (hasGetUserMedia()) {
+      const constraints = { audio: false, video: true };
+      const video = document.querySelector('video');
+
+      function handleSuccess(stream) {
+        console.log(stream);
+        video.srcObject = stream;
+
+        console.log(video.videoWidth);
+        console.log(video.videoHeight);
+        this.setState({
+          videoWidth: video.videoWidth,
+          videoHeight: video.videoHeight
+        });
+      }
+
+      function handleError(error) {
+        console.error('Rejected!', error);
+      }
+
+      navigator.mediaDevices.getUserMedia(constraints)
+        .then(handleSuccess)
+        .catch(handleError)
+
+    } else {
+      console.error("Get user media not supported :(");
+    }
+ 
+  }
+
+  handleClick() {
+    console.log('Button has been clicked.');
+    const video = document.querySelector('video');
+    const canvas = document.querySelector('canvas');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0);
+    this.setState({impath: canvas.toDataURL('image/jpeg')});
   }
 
   handleUpload(ev) {
@@ -23,11 +69,13 @@ class App extends Component {
   }
 
   render() {
-    console.log(this.state.impath);
     const ifNoImage = this.state.impath === '';
     const im = ifNoImage ? '' : <img alt="img" src={this.state.impath} />
     return (
       <div className="App">
+          <button onClick={this.handleClick}>snap</button>
+          <video autoPlay></video>
+          <canvas style={{display: 'none'}}></canvas>
           <input type="file" onChange={this.handleUpload}/>
           {im}
       </div>
