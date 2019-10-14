fb86079ff4670290cc3c2ee470fa335beaacc194
Ramon Blanquer <blanquer.ramon@gmail.com>
2019-10-15 00:49:54 +0200

    Prettier on the server.

--- frontend/server.js
+++ frontend/server.js
@@ -1,39 +1,38 @@
-const axios = require('axios');
-const express = require('express');
-const fs = require('fs');
-const multer = require('multer');
-const redis = require('redis');
+const axios = require("axios");
+const express = require("express");
+const fs = require("fs");
+const multer = require("multer");
+const redis = require("redis");
 
 // Setup redis client.
-const client = redis.createClient({ host: 'database' });
-client.on('error', err => console.error(err));
+const client = redis.createClient({ host: "database" });
+client.on("error", err => console.error(err));
 
 // Setup server and sockets.
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const websockets = {};
 
 // When client socket connects it will greet with the job id to keep track of.
-io.on('connection', (socket) => {
-  console.log('New socket connection established.');
-  socket.on('greet', jobId => {
-    // Add the socket to a global object so we can keep track of it and send 
+io.on("connection", socket => {
+  console.log("New socket connection established.");
+  socket.on("greet", jobId => {
+    // Add the socket to a global object so we can keep track of it and send
     // progress information through it every time redis pulls messages.
     websockets[jobId] = socket;
   });
 });
 
 // Check if this is a production deploy or development.
-const isDev = process.env.NODE_ENV !== 'production';
+const isDev = process.env.NODE_ENV !== "production";
 
 // Describe the callback that reads the progress.
-client.on('message', (chan, msg) => {
-
+client.on("message", (chan, msg) => {
   console.log(`Job ${chan} progress: ${msg} received from redis. Emitting.`);
   websockets[chan].emit(chan, msg); // Let React frontend know about it.
 
-  if (msg === 'FINISHED') {
+  if (msg === "FINISHED") {
     console.log(`Task completed. Unsubscribing from ${chan}.`);
     client.unsubscribe(chan);
 
@@ -44,15 +43,15 @@ client.on('message', (chan, msg) => {
 });
 
 // Express settings.
-app.use(express.json({ limit: '5mb' }));
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 // Setup webpack and live reloading if in dev mode.
 if (isDev) {
-  const webpack = require('webpack');
-  const webpackConfig = require('./webpack.config');
-  const webpackMiddleware = require('webpack-dev-middleware');
-  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const webpack = require("webpack");
+  const webpackConfig = require("./webpack.config");
+  const webpackMiddleware = require("webpack-dev-middleware");
+  const webpackHotMiddleware = require("webpack-hot-middleware");
 
   const webpackCompiler = webpack(webpackConfig);
   const wpmw = webpackMiddleware(webpackCompiler, {});
@@ -61,45 +60,45 @@ if (isDev) {
   const wphmw = webpackHotMiddleware(webpackCompiler);
   app.use(wphmw);
 } else {
-  app.use(express.static('dist'));
+  app.use(express.static("dist"));
 }
 
 // In charge to send the image to the backend. Returns a promise.
 // TODO: Move out hardcoded values to client code.
 function passImageToBackend(modelParameters, res) {
   axios
-    .post('http://api.dreambox.com/dream', modelParameters)
+    .post("http://api.dreambox.com/dream", modelParameters)
     .then(response => {
       client.subscribe(response.data);
       res.json({
         status: response.status,
-        message: 'all good',
-        body: response.data,
+        message: "all good",
+        body: response.data
       });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
         status: error.response.status,
         message: error.response.statusText,
-        body: error.response.data,
+        body: error.response.data
       });
     });
 }
 
 // Configure multer to handle file uploads.
 const storage = multer.diskStorage({
-  destination: (req, file, callback) => callback(null, '/uploads'),
-  filename: (req, file, callback) => callback(null, file.originalname),
+  destination: (req, file, callback) => callback(null, "/uploads"),
+  filename: (req, file, callback) => callback(null, file.originalname)
 });
 
-const upload = multer({ storage }).single('file');
+const upload = multer({ storage }).single("file");
 
 // When selecting a file from a folder and uploading it.
 // TODO: Request must have in the data object all the parameters for the model.
-app.post('/upload', (req, res) => {
-  upload(req, res, (err) => {
+app.post("/upload", (req, res) => {
+  upload(req, res, err => {
     if (err) {
-      res.end('Error uploading file.');
+      res.end("Error uploading file.");
     }
     passImageToBackend(req.file.path, res);
   });
@@ -107,38 +106,41 @@ app.post('/upload', (req, res) => {
 
 // When using the webcam (getting base64 data from canvas).
 // TODO: Request must have in the data object all the parameters for the model.
-app.post('/snap', (req, res) => {
+app.post("/snap", (req, res) => {
   fs.writeFile(
-    '/uploads/out.jpg',
-    req.body.image.replace(/^data:image\/png;base64,/, ''),
-    'base64',
+    "/uploads/out.jpg",
+    req.body.image.replace(/^data:image\/png;base64,/, ""),
+    "base64",
     err => console.error(err)
   );
   const modelParameters = {
     ...req.body.parameters,
-    image: '/uploads/out.jpg'
+    image: "/uploads/out.jpg"
   };
   passImageToBackend(modelParameters, res);
 });
 
-app.get('/models', (req, res) => {
-  axios.get('http://api.dreambox.com/models')
+app.get("/models", (req, res) => {
+  axios
+    .get("http://api.dreambox.com/models")
     .then(response => res.json(response.data))
     .catch(err => console.error(err));
 });
 
-app.get('/signature/:model', (req, res) => {
-  axios.get(`http://api.dreambox.com/signature/${req.param('model')}`)
+app.get("/signature/:model", (req, res) => {
+  axios
+    .get(`http://api.dreambox.com/signature/${req.param("model")}`)
     .then(response => res.json(response.data))
     .catch(err => console.error(err));
 });
 
-app.post('/cancel/:jobId', (req, res) => {
-  axios.post(`http://api.dreambox.com/cancel/${req.param('jobId')}`)
+app.post("/cancel/:jobId", (req, res) => {
+  axios
+    .post(`http://api.dreambox.com/cancel/${req.param("jobId")}`)
     .then(response => res.json(response.data))
     .catch(err => console.error(err));
 });
 
 http.listen(3000, () => {
-  console.log(`Dreambox app listening! NODE_ENV: ${process.env.NODE_ENV}`)
+  console.log(`Dreambox app listening! NODE_ENV: ${process.env.NODE_ENV}`);
 });
