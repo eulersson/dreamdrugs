203ce730b59a143d6f863ef2539bf392e7fe7691
Ramon Blanquer <blanquer.ramon@gmail.com>
2020-08-09 00:58:29 +0200

    Use frontend/.env. Pass BACKEND_DOMAIN to React code.

--- frontend/webpack.production.config.js
+++ frontend/webpack.production.config.js
@@ -2,36 +2,38 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: {
-    app: './src/client'
-  },
-  devtool: 'cheap-source-map',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?/,
-        loaders: 'babel-loader',
-        include: __dirname,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ] 
-  },
-  plugins: [
-    new CleanWebpackPlugin({ verbose: true }),
-    new HtmlWebpackPlugin({
-      template: './src/server/views/index.html',
-      filename: 'index.html',
-      inject: 'body'
-    })
-  ],
-  mode: 'production'
-};
+module.exports = function(env, argv) {
+  return {
+    entry: {
+      app: './src/client'
+    },
+    devtool: 'cheap-source-map',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?/,
+          loaders: 'babel-loader',
+          include: __dirname,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ] 
+    },
+    plugins: [
+      new CleanWebpackPlugin({ verbose: true }),
+      new HtmlWebpackPlugin({
+        template: './src/server/views/index.html',
+        filename: 'index.html',
+        inject: 'body'
+      })
+    ],
+    mode: 'production'
+  }
+}
